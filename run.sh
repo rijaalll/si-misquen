c98dbe3c0b75a92d5b4c3e00c6e480c97b18eae0
zaa <146299812+rijaalll@users.noreply.github.com>
2025-06-24 22:16:03 +0700

    add run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
 # sh run.sh
+
+# echo "Menjalankan npm install" <- hilangkan "#" jika pertama kali menjalankan 
+# npm install                 <----
 
 echo "Menjalankan project"
 npm run dev
